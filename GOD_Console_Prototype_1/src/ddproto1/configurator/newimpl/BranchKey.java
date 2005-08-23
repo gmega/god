d63b07744d9eb2dbd9b4701e6f1ca42bb16ce645
gmega <giuliano.mega@gmail.com>
2005-08-23 22:37:23 +0000

    Failed attempt. Commiting just not to lose this version.
    git-svn-id: svn://localhost/tmp/repo1/trunk@19 70b6eb62-b118-4aea-92db-fdb7de3615d6

--- GOD_Console_Prototype_1/src/ddproto1/configurator/newimpl/BranchKey.java
+++ GOD_Console_Prototype_1/src/ddproto1/configurator/newimpl/BranchKey.java
@@ -5,29 +5,62 @@
  */
 package ddproto1.configurator.newimpl;
 
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
-public class BranchKey{
-    private String key;
-    private String val;
+import ddproto1.util.collection.AbstractMultiMap;
+
+
+public class BranchKey <K, V>{
+    
+    public static final boolean IS_KEY = true;
+    public static final boolean IS_VALUE = false;
+    
+    private K key;
+    private V val;
     
     private int hashCode;
     
-    public BranchKey(String key, String val){
+    private List<IRemovalStrategy> removalList = new LinkedList<IRemovalStrategy>();
+    
+    public BranchKey(K key, V val){
         this.key = key;
         this.val = val;
-        this.hashCode = (key + val).hashCode();
+        this.hashCode = key.hashCode() + val.hashCode();
     }
     
-    public String getKey() { return key; }
+    public K getKey() { return key; }
     
-    public String getValue() { return val; }
+    public V getValue() { return val; }
+    
+    public void invalidate(){
+        for(IRemovalStrategy strategy : removalList)
+            strategy.remove();
+    }
+    
+    public String toString(){
+        return "<" + key + "," + val + ">";
+    }
     
     public boolean isSatisfiedBy(Map<String, String> valueMap){
         String value = valueMap.get(key);
         if(value == null) return false;
         if(value.equals(val)) return true;
         return false;
+    }
+    
+    public void addTo(Map target, Object parameter, boolean isKey){
+        removalList.add(new MapRemovalStrategy(target, isKey, parameter));
+    }
+    
+    public void addTo(Collection target){
+        removalList.add(new CollectionRemovalStrategy(target));
+    }
+    
+    public void addTo(AbstractMultiMap target, String classKey, boolean isKey){
+        removalList.add(new MultiMapRemovalStrategy(target, classKey, isKey));
     }
     
     public int hashCode(){
@@ -38,5 +71,63 @@ public class BranchKey{
         if(!(anObject instanceof BranchKey)) return false;
         BranchKey other = (BranchKey)anObject;
         return key.equals(other.key) && val.equals(other.val);
+    }
+    
+    private interface IRemovalStrategy{
+        public void remove();
+    }
+    
+    private class MapRemovalStrategy implements IRemovalStrategy{
+        
+        private Map target;
+        private boolean isKey;
+        private Object parameter;
+
+        public MapRemovalStrategy(Map target, boolean isKey, Object parameter){
+            if(isKey) target.put(BranchKey.this, parameter);
+            else target.put(parameter, BranchKey.this);
+            this.target = target;
+            this.isKey = isKey;
+        }
+        
+        public void remove() {
+            if(isKey) target.remove(BranchKey.this);
+            else target.remove(parameter);
+        }
+        
+    }
+    
+    private class CollectionRemovalStrategy implements IRemovalStrategy{
+        
+        private Collection which;
+        
+        public CollectionRemovalStrategy(Collection c){
+            c.add(BranchKey.this);
+            this.which = c;
+        }
+
+        public void remove() {
+            assert(which.contains(BranchKey.this));
+            which.remove(BranchKey.this);
+        }
+    }
+    
+    private class MultiMapRemovalStrategy implements IRemovalStrategy{
+        
+        private AbstractMultiMap which;
+        private Object classKey;
+        private boolean isKey;
+        
+        public MultiMapRemovalStrategy(AbstractMultiMap c, Object classKey, boolean isKey){
+            if(isKey) c.add(BranchKey.this, classKey);
+            else c.add(classKey, BranchKey.this);
+            this.isKey = isKey;
+        }
+
+        public void remove() {
+            if(isKey) which.removeClass(BranchKey.this);
+            else which.remove(classKey, BranchKey.this);
+        }
+        
     }
 }
