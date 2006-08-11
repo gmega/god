4c76114668f8537a2a9937d495c5c610a6f703f7
gmega <giuliano.mega@gmail.com>
2006-08-11 23:59:14 +0000

    - Added missing attributes.
    git-svn-id: svn://localhost/tmp/repo1/trunk@217 70b6eb62-b118-4aea-92db-fdb7de3615d6

--- GOD_Console_Prototype_1/src/test/ddproto1/launcher/procserver/test/LocalAgentCommandLineTest.java
+++ GOD_Console_Prototype_1/src/test/ddproto1/launcher/procserver/test/LocalAgentCommandLineTest.java
@@ -25,14 +25,24 @@ public class LocalAgentCommandLineTest extends TestCase {
 		ICommandLine lacl = (ICommandLine) sLocator.incarnate(_lacl);
 		
 		lacl.setAttribute(IConfigurationConstants.GUID_ATTRIBUTE, "0");
+        lacl.setAttribute(IConfigurationConstants.CONN_POOL_SIZE, "1");
+        lacl.setAttribute(IConfigurationConstants.GLOBAL_AGENT_ADDRESS, "127.0.0.1");
+        lacl.setAttribute(IConfigurationConstants.CDWP_PORT, "25");
 		
 		String [] cline = lacl.renderCommandLine();
 		
 		String [] oracleLine = {
 				"java", "-D"+IConfigurationConstants.LOCAL_AGENT_GID_OPT+"=0",
+                "-D" + IConfigurationConstants.LOCAL_AGENT_GA_ADDRESS_OPT + "=127.0.0.1:25",
+                "-D" + IConfigurationConstants.LOCAL_AGENT_CONNPOOL_OPT + "=1",
 				"ddproto1.launcher.procserver.test.testapp.TestApplication"
 		};
-		
+        
+        for(String param : cline) System.out.print(param + " ");
+        System.out.println("");
+        for(String param : oracleLine) System.out.print(param + " "); 
+        System.out.println("");
+        
 		assertTrue(cline.length == oracleLine.length);
 		
 		for(int i = 0; i < cline.length; i++){
