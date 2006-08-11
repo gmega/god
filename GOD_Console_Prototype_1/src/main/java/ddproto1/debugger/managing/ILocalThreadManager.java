7685d3b7dcc7d43d1f6a3efdecda9a686bc568f3
gmega <giuliano.mega@gmail.com>
2006-08-11 23:40:01 +0000

    - Added comments to interface methods.
    git-svn-id: svn://localhost/tmp/repo1/trunk@194 70b6eb62-b118-4aea-92db-fdb7de3615d6

--- GOD_Console_Prototype_1/src/main/java/ddproto1/debugger/managing/ILocalThreadManager.java
+++ GOD_Console_Prototype_1/src/main/java/ddproto1/debugger/managing/ILocalThreadManager.java
@@ -7,7 +7,29 @@ package ddproto1.debugger.managing;
 
 import ddproto1.debugger.managing.tracker.ILocalThread;
 
+/**
+ * Thread manager - interface for accessing the local threads in a 
+ * node proxy.
+ * 
+ * @author giuliano
+ *
+ */
 public interface ILocalThreadManager extends IThreadManager{
+    /**
+     * Returns a snapshot of the currently active threads. 
+     * Note that the snapshot is not reliable - the list of
+     * active threads is in continuous change.
+     * 
+     * @return
+     */
     public ILocalThread [] getLocalThreads();
+    
+    /**
+     * Returns a local thread with a specific global id.
+     * 
+     * @param uuid the ID of the local thread
+     * @return the local thread with corresponding global ID, 
+     * or <b>null</b> if no such thread exists.
+     */
     public ILocalThread    getLocalThread(Integer uuid);
 }
