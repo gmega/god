ef155639ea0d05f8c1f40de27a07b644ceff9095
gmega <giuliano.mega@gmail.com>
2006-08-11 23:36:56 +0000

    - Documented the interface. - Changed getLIThread to getAdapterForThread, as it reflects better what this method really does.
    git-svn-id: svn://localhost/tmp/repo1/trunk@190 70b6eb62-b118-4aea-92db-fdb7de3615d6

--- GOD_Console_Prototype_1/src/main/java/ddproto1/debugger/managing/IJavaThreadManager.java
+++ GOD_Console_Prototype_1/src/main/java/ddproto1/debugger/managing/IJavaThreadManager.java
@@ -12,23 +12,69 @@ package ddproto1.debugger.managing;
 import com.sun.jdi.ThreadReference;
 
 /**
- * @author giuliano
- *
- * This interface is going through a process of conversion to the 
- * Eclipse debug model, which is language-independent. 
- *
+ * Java-specific version of ILocalThreadManager
  */
 public interface IJavaThreadManager extends ILocalThreadManager{
-
-    /** Those methods should be converted to use IThreads only. */
+    /**
+     * Sets the thread passed as a parameter to be the last thread
+     * in which a relevant event has occurred.
+     * 
+     * @param tr
+     * @deprecated This made sense in the console version, but doesn't
+     * make sense in the Eclipse version.
+     */
     public void setCurrentThread(ThreadReference tr);
-    public ThreadReference findThreadById(long uid);
-    public ThreadReference findThreadByUUID(int uuid);
-    public ThreadReference findThreadByUUID(Integer uuid);
-    public ThreadReference getCurrentThread();
-    public Integer         getThreadUUID(ThreadReference tr);
-    public JavaThread      getLIThread(ThreadReference tr);
-    public void            notifyVMSuspend();
-
     
+    /**
+     * Returns the  
+     * 
+     * @return
+     * @deprecated
+     */
+    public ThreadReference getCurrentThread();
+    
+    /**
+     * Returns the JDI reference with a specific local id. This ID is JVM 
+     * assigned, it might be the same for two threads belonging to different 
+     * machines.   
+     * 
+     * @param gid The global id of the thread.
+     * @return the thread reference matching the id, or <b>null</b> if
+     * no such thread exists.
+     */
+    public ThreadReference findThreadById(long gid);
+    
+    /**
+     * Returns a JDI reference to a thread with a specific global id.
+     * This ID is globally unique.
+     * 
+     * @param uuid
+     * @return 
+     */
+    public ThreadReference findThreadByUUID(int uuid);
+    
+    /**
+     * Returns the globally unique ID of a given JDI thread.
+     * 
+     * @param tr
+     * @return
+     */
+    public Integer getThreadUUID(ThreadReference tr);
+
+    /**
+     * Since com.sun.jdi.ThreadReference doesn't implement IAdaptable,
+     * this method adapts ThreadReference to JavaThread. We should 
+     * use PlatformObject to do this in the future and deprecate this
+     * method.
+     * 
+     * @param tr
+     * @return
+     */
+    public JavaThread getAdapterForThread(ThreadReference tr);
+    
+    /**
+     * Notifies this thread manager that the Virtual Machine
+     * has been suspended.
+     */
+    public void notifyVMSuspend();
 }
