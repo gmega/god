031ffce2f255c1c4d5f4b74da31a209bca3b7e4d
gmega <giuliano.mega@gmail.com>
2006-08-22 22:17:15 +0000

    - Added JDIEventProcessorTrait#process can now throw exception. - Added exception handling to event processing loop.
    git-svn-id: svn://localhost/tmp/repo1/trunk@253 70b6eb62-b118-4aea-92db-fdb7de3615d6

--- GOD_Console_Prototype_1/src/main/java/ddproto1/util/traits/JDIEventProcessorTrait.java
+++ GOD_Console_Prototype_1/src/main/java/ddproto1/util/traits/JDIEventProcessorTrait.java
@@ -1,5 +1,7 @@
 package ddproto1.util.traits;
 
+import org.apache.log4j.Logger;
+
 import com.sun.jdi.event.Event;
 
 import ddproto1.debugger.eventhandler.processors.AbstractEventProcessor;
@@ -7,8 +9,10 @@ import ddproto1.debugger.eventhandler.processors.IJDIEventProcessor;
 
 public class JDIEventProcessorTrait implements IJDIEventProcessor{
 	
+    private static final Logger logger = Logger.getLogger(JDIEventProcessorTrait.class);
+    
 	public interface JDIEventProcessorTraitImplementor{
-		public void specializedProcess(Event request);
+		public void specializedProcess(Event request) throws Exception;
 		public void next(IJDIEventProcessor next);
 		public IJDIEventProcessor next();
 		public boolean enabled();
@@ -21,20 +25,26 @@ public class JDIEventProcessorTrait implements IJDIEventProcessor{
 		_this = implementor;
 	}
 	
-	public void process(Event request){
+	public void process(Event request)
+        throws Exception
+    {
         if(_this.enabled())
             _this.specializedProcess(request);
         if(_this.next() == null) return;
         
         // This eliminates recursion when possible
         for(IJDIEventProcessor el = _this.next(); el != null; el = (AbstractEventProcessor)el.getNext()){
-            if(el instanceof JDIEventProcessorTraitImplementor){
-                JDIEventProcessorTraitImplementor ep = (JDIEventProcessorTraitImplementor)el;
-                if(ep.enabled())
-                    ep.specializedProcess(request);
-            }else{
-                el.process(request);
-                break;
+            try{
+                if(el instanceof JDIEventProcessorTraitImplementor){
+                    JDIEventProcessorTraitImplementor ep = (JDIEventProcessorTraitImplementor)el;
+                    if(ep.enabled())
+                        ep.specializedProcess(request);
+                }else{
+                    el.process(request);
+                    break;
+                }
+            }catch(Exception ex){
+                logger.error("Caught error in event processing loop.", ex);
             }
         }
 	}
