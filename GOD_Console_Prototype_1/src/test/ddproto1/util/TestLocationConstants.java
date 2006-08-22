60e3b6a12468a74ad4dc16faffe29006afec6f3b
gmega <giuliano.mega@gmail.com>
2006-08-22 22:20:37 +0000

    - Added location constants to supply the remaining of the distributed thread tests.
    git-svn-id: svn://localhost/tmp/repo1/trunk@257 70b6eb62-b118-4aea-92db-fdb7de3615d6

--- GOD_Console_Prototype_1/src/test/ddproto1/util/TestLocationConstants.java
+++ GOD_Console_Prototype_1/src/test/ddproto1/util/TestLocationConstants.java
@@ -8,12 +8,16 @@ package ddproto1.util;
 public interface TestLocationConstants {
     /** Location constants for DistributedThreadTest */
     public static final String DT_TEST_SERVER_BKP_CL = "implementation.QuoterImpl";
-    public static final int DT_TEST_FINAL_BKP_LINE = 63;
+    public static final int DT_TEST_FINAL_BKP_LINE = 94;
     public static final int DT_TEST_SPIN_BKP_LINE = 61;
+    public static final int DT_TEST_CHAINED_UPCALL_LINE = 88;
 
     public static final String DT_TEST_CLIENT_BKP_CL = "Client";
     public static final int DT_TEST_CLIENT_BKP_LINE = 85;
     public static final String DT_TEST_STEP_INTO_THREAD_NAME = "ACME";
+    
+    public static final int DT_TEST_FIRST_QUOTERIMPL_EXECUTABLE_LINE = 57;
+    public static final int DT_TEST_CLIENT_EXCEPTIONHANDLER_LINE = 89;
     
     /** Location constants for CondensedSingleNodeTest */
     public static final String CSN_TEST_LOOP_BKP_CL = "SimpleClass";
