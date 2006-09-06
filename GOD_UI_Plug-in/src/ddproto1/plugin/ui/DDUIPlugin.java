9a3b498966c4fb9b51fa33867627572c04305eeb
gmega <giuliano.mega@gmail.com>
2006-09-06 04:04:59 +0000

    - Removed messagebox setup from UI plugin. It's now at the base plugin.
    git-svn-id: svn://localhost/tmp/repo1/trunk@262 70b6eb62-b118-4aea-92db-fdb7de3615d6

--- GOD_UI_Plug-in/src/ddproto1/plugin/ui/DDUIPlugin.java
+++ GOD_UI_Plug-in/src/ddproto1/plugin/ui/DDUIPlugin.java
@@ -7,53 +7,22 @@ package ddproto1.plugin.ui;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.Iterator;
 import java.util.MissingResourceException;
 import java.util.Properties;
 import java.util.ResourceBundle;
 
 import org.apache.log4j.Logger;
-import org.apache.commons.collections.BidiMap;
-import org.apache.commons.collections.bidimap.DualHashBidiMap;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Preferences;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.Preferences.IPropertyChangeListener;
-import org.eclipse.core.runtime.Preferences.PropertyChangeEvent;
-import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
-import org.xml.sax.SAXException;
 
 import com.tools.logging.PluginLogManager;
-
-import ddproto1.GODBasePlugin;
-import ddproto1.configurator.IObjectSpec;
-import ddproto1.configurator.IObjectSpecType;
-import ddproto1.configurator.ISpecLoader;
-import ddproto1.configurator.ISpecType;
-import ddproto1.configurator.ObjectSpecStringfier;
-import ddproto1.configurator.SpecLoader;
-import ddproto1.configurator.SpecNotFoundException;
-import ddproto1.configurator.commons.IConfigurationConstants;
-import ddproto1.exception.DuplicateSymbolException;
-import ddproto1.exception.NoSuchSymbolException;
-import ddproto1.exception.commons.AttributeAccessException;
-import ddproto1.exception.commons.IllegalAttributeException;
-import ddproto1.exception.commons.InvalidAttributeValueException;
-import ddproto1.exception.commons.NestedRuntimeException;
-import ddproto1.interfaces.IMessageBox;
-import ddproto1.plugin.ui.launching.FSImplementationScanner;
-import ddproto1.plugin.ui.launching.IImplementationScanner;
-import ddproto1.plugin.ui.launching.listers.BundleEntryLister;
-import ddproto1.util.Base64Encoder;
-import ddproto1.util.MessageHandler;
 
 public class DDUIPlugin extends AbstractUIPlugin{
     
@@ -63,8 +32,10 @@ public class DDUIPlugin extends AbstractUIPlugin{
     private ResourceBundle resourceBundle;
     //Logger (lazily initialized) 
     private Logger logger;
+    //Logger manager.
+    private PluginLogManager logManager;
     
-    private PluginLogManager  logManager;
+    private Properties fPreferencePages;
 
     /**
      * The constructor.
@@ -73,7 +44,6 @@ public class DDUIPlugin extends AbstractUIPlugin{
         super();
         plugin = this;
     }
-
     
     public static DDUIPlugin getDefault(){
         return plugin;
@@ -86,7 +56,6 @@ public class DDUIPlugin extends AbstractUIPlugin{
     public void start(BundleContext context) throws Exception {
         super.start(context);
         this.initManagers();
-        this.setMessageBoxes();
     }
 
     
@@ -116,6 +85,18 @@ public class DDUIPlugin extends AbstractUIPlugin{
             return key;
         }
     }
+    
+    public synchronized Properties getPreferencePagesSpec(){
+        if(fPreferencePages == null){
+            Preferences prefs = getPluginPreferences();
+            String prefPageEntry = prefs.getString(
+                    UIDebuggerConstants.PREFERENCE_PAGES_FILE);
+            fPreferencePages = 
+                loadFromRoot(prefPageEntry, "preference pages");
+        }
+        
+        return fPreferencePages;
+    }
 
     public ResourceBundle getResourceBundle() {
         try {
@@ -139,44 +120,16 @@ public class DDUIPlugin extends AbstractUIPlugin{
      * @return the image descriptor
      */
     public static ImageDescriptor getImageDescriptor(String path) {
-        return AbstractUIPlugin.imageDescriptorFromPlugin("GOD_Prototype_1_UI_Plug_in", path);
-    }
-    
-    
-    private void setMessageBoxes(){
-        IMessageBox stdout = new IMessageBox(){
-            public void println(String s) { logger.info(s); }
-            public void print(String s) { logger.info(s); }
-        };
-        
-        IMessageBox stderr = new IMessageBox(){
-            public void println(String s) { logger.error(s); }
-            public void print(String s) { logger.error(s); }
-        };
-        
-        IMessageBox warn = new IMessageBox(){
-            public void println(String s) { logger.warn(s); }
-            public void print(String s) { logger.warn(s); }
-        };
-        
-        IMessageBox debug = new IMessageBox(){
-            public void println(String s) { logger.debug(s); }
-            public void print(String s) { logger.debug(s); }
-        };
-
-        
-        MessageHandler mh = MessageHandler.getInstance();
-        mh.setWarningOutput(warn);
-        mh.setStandardOutput(stdout);
-        mh.setErrorOutput(stderr);
-        mh.setDebugOutput(debug);
+        if(plugin == null)
+            return null;
+        return AbstractUIPlugin.imageDescriptorFromPlugin(plugin.getBundle().getSymbolicName(), path);
     }
     
     private void configureLogManager() {
         Bundle bundle = DDUIPlugin.this.getBundle();
         Preferences prefs = DDUIPlugin.this.getPluginPreferences();
         URL url = bundle.getEntry(
-                "/" + prefs.getString(IDebuggerConstantsUI.LOG_PROPERTIES_FILE));
+                "/" + prefs.getString(UIDebuggerConstants.LOG_PROPERTIES_FILE));
         InputStream propertiesInputStream = null;
         try{
             if(url != null) propertiesInputStream = url.openStream();
@@ -190,7 +143,7 @@ public class DDUIPlugin extends AbstractUIPlugin{
             }
         }catch(IOException ex){
             DDUIPlugin.this.getLog().log(new Status(IStatus.ERROR, bundle.getSymbolicName(), IStatus.ERROR, 
-                    "Failed to read log configuration file " + IDebuggerConstantsUI.LOG_PROPERTIES_FILE + ". Will" +
+                    "Failed to read log configuration file " + UIDebuggerConstants.LOG_PROPERTIES_FILE + ". Will" +
                     " attempt to use default configurations.", ex));
         }
         this.logManager = new PluginLogManager(DDUIPlugin.this, props);
@@ -207,4 +160,30 @@ public class DDUIPlugin extends AbstractUIPlugin{
                 plugin.getBundle().getSymbolicName(), IStatus.ERROR, reason, t));
     }
     
+    public static void logError(String error, Throwable t){
+        DDUIPlugin.getDefault().getLog().log(
+                new Status(IStatus.ERROR,
+                           DDUIPlugin.getDefault().getBundle().getSymbolicName(),
+                           IStatus.ERROR,
+                               error,
+                            t));
+    }
+    
+    public static Properties loadFromRoot(String propfileName, String description){
+        Bundle bundle = DDUIPlugin.getDefault().getBundle();
+        URL fileURL = bundle.getEntry("/" + propfileName);
+        if(fileURL == null){
+            logError("Could not locate " + description + " " + propfileName, null);
+            return null;
+        }
+        
+        Properties properties = new Properties();
+        try{
+            properties.load(fileURL.openStream());
+        }catch(Exception ex){
+            DDUIPlugin.logError("Error while loading bootstrap property file.", ex);
+        }
+        
+        return properties;
+    }
 }
