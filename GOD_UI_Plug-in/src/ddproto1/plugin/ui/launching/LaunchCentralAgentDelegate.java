8e2b21d504041c76e436e5791b247c1976689bbe
gmega <giuliano.mega@gmail.com>
2006-09-06 04:09:46 +0000

    - Adapted the delegate to use the GODBasePlugin preferences to communicate configuration information to the central agent and process server manager.
    git-svn-id: svn://localhost/tmp/repo1/trunk@268 70b6eb62-b118-4aea-92db-fdb7de3615d6

--- GOD_UI_Plug-in/src/ddproto1/plugin/ui/launching/LaunchCentralAgentDelegate.java
+++ GOD_UI_Plug-in/src/ddproto1/plugin/ui/launching/LaunchCentralAgentDelegate.java
@@ -7,6 +7,8 @@ package ddproto1.plugin.ui.launching;
 
 import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Preferences;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationType;
@@ -18,9 +20,7 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
-import ddproto1.configurator.IObjectSpec;
 import ddproto1.configurator.commons.IConfigurationConstants;
-import ddproto1.exception.commons.AttributeAccessException;
 import ddproto1.plugin.ui.DDUIPlugin;
 
 public class LaunchCentralAgentDelegate implements IWorkbenchWindowActionDelegate, IConfigurationConstants{
@@ -30,30 +30,21 @@ public class LaunchCentralAgentDelegate implements IWorkbenchWindowActionDelegat
     public void run(IAction action) {
         try{
             
-            /** 
-             * We pass the root node configuration attributes to the configuration copy.
-             */
             DebugPlugin plugin = DebugPlugin.getDefault();
             ILaunchConfigurationType gacType = 
                 plugin.getLaunchManager().getLaunchConfigurationType(IConfigurationConstants.ID_GLOBAL_AGENT_APPLICATION);
             ILaunchConfigurationWorkingCopy workingCopy = gacType.newInstance(null, CENTRAL_AGENT_CONFIG_NAME);
+
+            /** Create launch configuration from preferences */
+            Preferences prefs = DDUIPlugin.getDefault().getPluginPreferences(); 
+            String [] setPrefs = prefs.propertyNames();
             
-            IObjectSpec rootSpec = DDUIPlugin.getDefault().getConfigurationManager().getRootSpec();
-            
-            try{
-                /** Please note that the attributes are hardwired, there's no translation level. Exercise 
-                 * care if you change the xml specs. */
-                workingCopy.setAttribute(THREAD_POOL_SIZE, rootSpec.getAttribute(THREAD_POOL_SIZE));
-                workingCopy.setAttribute(MAX_QUEUE_LENGTH, rootSpec.getAttribute(MAX_QUEUE_LENGTH));
-                workingCopy.setAttribute(CDWP_PORT, rootSpec.getAttribute(CDWP_PORT));
-            }catch(AttributeAccessException ex){
-                logger.error("Failed to launch central agent due to a configuration error.", ex);
-                return;
+            for(String setPref : setPrefs){
+                workingCopy.setAttribute(setPref, prefs.getString(setPref));
             }
             
             ILaunchConfiguration config = workingCopy.doSave();
-            DebugUITools.launch(config, ILaunchManager.DEBUG_MODE);
-            
+            config.launch(ILaunchManager.DEBUG_MODE, new NullProgressMonitor());
         }catch(CoreException ex){
             logger.error("Error while launching the central agent.", ex);
         }
