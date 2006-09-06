fc0f183c6941925ffd2a40a8eea7cd39bfa9fd4c
gmega <giuliano.mega@gmail.com>
2006-09-06 04:14:36 +0000

    Adapted this "test" to run as a plug-in test. Still doesn't work, though.
    git-svn-id: svn://localhost/tmp/repo1/trunk@274 70b6eb62-b118-4aea-92db-fdb7de3615d6

--- GOD_UI_Plug-in/src/ddproto1/plugin/ui/test/ConfigurationPanelPrototypeTest.java
+++ GOD_UI_Plug-in/src/ddproto1/plugin/ui/test/ConfigurationPanelPrototypeTest.java
@@ -7,6 +7,8 @@ package ddproto1.plugin.ui.test;
 
 import java.io.IOException;
 
+import junit.framework.TestCase;
+
 import org.eclipse.jface.window.ApplicationWindow;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
@@ -14,43 +16,52 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 
 import ddproto1.configurator.IObjectSpec;
-import ddproto1.plugin.ui.DDUIPlugin;
 import ddproto1.plugin.ui.launching.ConfigurationPanel;
-import ddproto1.test.XMLParserTest;
 
-
-public class ConfigurationPanelPrototypeTest extends ApplicationWindow{
+/**
+ * This isn't really a test, but a support class to test the 
+ * configuration view.  
+ * 
+ * @author giuliano
+ *
+ */
+public class ConfigurationPanelPrototypeTest extends TestCase{
 
     private IObjectSpec root;
 
-    public ConfigurationPanelPrototypeTest() {
-        super(null);
-        XMLParserTest parserTest = new XMLParserTest();
-        parserTest.setUp();
-        parserTest.testParseConfig();
-        this.root = parserTest.getRoot();
-    }
-
-    /* (non-Javadoc)
-     * @see org.eclipse.jface.window.Window#createContents(org.eclipse.swt.widgets.Composite)
-     */
-    @Override
-    protected Control createContents(Composite parent) {
-        this.getShell().setText("Configuration Window");
-        ConfigurationPanel proto = new ConfigurationPanel(parent, SWT.NONE);
-        System.err.println(System.getProperty("java.class.path"));
-        try{
-            proto.setObjectSpecRoot(root.getChildren().get(0).getChildren().get(0));
-        }catch(IOException ex){ ex.printStackTrace(); } 
-        parent.setSize(700, 400);
-        return parent;
-    }
-    
-    public static void main(String [] args){
-        ConfigurationPanelPrototypeTest cppt = new ConfigurationPanelPrototypeTest();
+    public void testConfigPanel()
+        throws Exception
+    {
+        ConfigPanel cppt = new ConfigPanel();
         cppt.setBlockOnOpen(true);
         cppt.open();
         Display.getCurrent().dispose();
+    }
+    
+    private class ConfigPanel extends ApplicationWindow {
+
+        public ConfigPanel(){
+            super(null);
+        }
+        /*
+         * (non-Javadoc)
+         * 
+         * @see org.eclipse.jface.window.Window#createContents(org.eclipse.swt.widgets.Composite)
+         */
+        @Override
+        protected Control createContents(Composite parent) {
+            this.getShell().setText("Configuration Window");
+            ConfigurationPanel proto = new ConfigurationPanel(parent, SWT.NONE);
+            System.err.println(System.getProperty("java.class.path"));
+            try {
+                proto.setObjectSpecRoot(root.getChildren().get(0).getChildren()
+                        .get(0));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            parent.setSize(700, 400);
+            return parent;
+        }
     }
    
 }
