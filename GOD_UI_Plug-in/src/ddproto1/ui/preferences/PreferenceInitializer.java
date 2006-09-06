0ac4e6147ad3ea7ed7502902df9c623ed5e70d39
gmega <giuliano.mega@gmail.com>
2006-09-06 04:19:03 +0000

    - Initialization algorithm now reads bootstrap properties and base plug-in properties for defaults. UI plugin preferences became a duplicate of base plug-in preferences plus some extra configuration information.
    git-svn-id: svn://localhost/tmp/repo1/trunk@277 70b6eb62-b118-4aea-92db-fdb7de3615d6

--- GOD_UI_Plug-in/src/ddproto1/ui/preferences/PreferenceInitializer.java
+++ GOD_UI_Plug-in/src/ddproto1/ui/preferences/PreferenceInitializer.java
@@ -2,38 +2,52 @@ package ddproto1.ui.preferences;
 
 import java.util.Properties;
 
+import org.eclipse.core.runtime.Preferences;
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
-import org.eclipse.jface.preference.IPreferenceStore;
 
+import ddproto1.GODBasePlugin;
 import ddproto1.configurator.commons.IConfigurationConstants;
 import ddproto1.plugin.ui.DDUIPlugin;
-import ddproto1.plugin.ui.IDebuggerConstantsUI;
+import ddproto1.plugin.ui.UIDebuggerConstants;
 
 /**
  * Class used to initialize default preference values.
  */
 public class PreferenceInitializer extends AbstractPreferenceInitializer implements IConfigurationConstants {
-
+    
 	/*
 	 * (non-Javadoc)
 	 * 
 	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
 	 */
 	public void initializeDefaultPreferences() {
-        DDUIPlugin plugin = DDUIPlugin.getDefault();
-        
-		IPreferenceStore store = plugin.getPreferenceStore();
-        Properties bootstrap = plugin.getConfigurationManager().getBootstrapProperties();
-        
-        /** Applies the bootstrap properties. */
-        String tocSubpath = bootstrap.getProperty(IDebuggerConstantsUI.TOC_SUBPATH);
-		store.setDefault(IDebuggerConstantsUI.TOC_SUBPATH, tocSubpath);
-		store.setDefault(IDebuggerConstantsUI.SPECS_SUBPATH_LIST, tocSubpath);
-        
-        store.setDefault(GLOBAL_AGENT_ADDRESS, IDebuggerConstantsUI.DEFAULT_GA_ADDRESS);
-        store.setDefault(CDWP_PORT, IDebuggerConstantsUI.DEFAULT_CDWP_PORT);
-        store.setDefault(MAX_QUEUE_LENGTH, IDebuggerConstantsUI.DEFAULT_CONN_QUEUE);
-        store.setDefault(THREAD_POOL_SIZE, IDebuggerConstantsUI.DEFAULT_THREAD_POOL);
+        loadBootstrapProperties();
+        getPropertiesFromBasePlugin();
 	}
-
+    
+    private void loadBootstrapProperties(){
+        Properties bStrap = 
+            DDUIPlugin.loadFromRoot(
+                UIDebuggerConstants.BOOTSTRAP_PROPERTIES_FILE,
+                "bootstrap properties file");
+        
+        Preferences prefs = DDUIPlugin.getDefault().getPluginPreferences();
+        
+        for(Object key : bStrap.keySet()){
+            String value = (String)bStrap.get(key);
+            prefs.setDefault((String)key, value);
+        }
+    }
+    
+    private void getPropertiesFromBasePlugin(){
+        Properties props = DDUIPlugin.getDefault().getPreferencePagesSpec();
+        Preferences basePrefs = GODBasePlugin.getDefault().getPluginPreferences();
+        Preferences ourPrefs = DDUIPlugin.getDefault().getPluginPreferences();
+        for(Object propKey : props.keySet()){
+            String key = (String)propKey;
+            String baseValue = basePrefs.getString(key);
+            if(baseValue.equals("")) continue;
+            ourPrefs.setDefault(key, baseValue);
+        }
+    }
 }
