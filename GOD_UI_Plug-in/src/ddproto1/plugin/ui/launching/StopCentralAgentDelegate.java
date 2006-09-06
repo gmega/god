bb7db4efa077b9b9db3715903f7c003b09235276
gmega <giuliano.mega@gmail.com>
2006-09-06 04:10:19 +0000

    - Initial implementation of "stop central agent" button.
    git-svn-id: svn://localhost/tmp/repo1/trunk@269 70b6eb62-b118-4aea-92db-fdb7de3615d6

--- GOD_UI_Plug-in/src/ddproto1/plugin/ui/launching/StopCentralAgentDelegate.java
+++ GOD_UI_Plug-in/src/ddproto1/plugin/ui/launching/StopCentralAgentDelegate.java
@@ -5,31 +5,46 @@
  */
 package ddproto1.plugin.ui.launching;
 
+import org.apache.log4j.Logger;
+import org.eclipse.debug.core.DebugException;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.model.IDebugTarget;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
+import ddproto1.configurator.commons.IConfigurationConstants;
+import ddproto1.util.MessageHandler;
+
 public class StopCentralAgentDelegate implements IWorkbenchWindowActionDelegate {
 
+    private static final Logger logger = MessageHandler.getInstance().getLogger(StopCentralAgentDelegate.class);
+    
     public void run(IAction action) {
-        // TODO Auto-generated method stub
-
-    }
-
-    public void selectionChanged(IAction action, ISelection selection) {
-        // TODO Auto-generated method stub
-
-    }
-
-    public void dispose() {
-        // TODO Auto-generated method stub
+        /** Finds the central agent debug target */
+        IDebugTarget [] targets = DebugPlugin.getDefault().getLaunchManager().getDebugTargets();
+        IDebugTarget caTarget= null;
+        for(IDebugTarget target : targets){
+            try{
+                if(target.getLaunch().getLaunchConfiguration().getName().equals(IConfigurationConstants.CENTRAL_AGENT_CONFIG_NAME) &&
+                        target.getName().equals(IConfigurationConstants.CENTRAL_AGENT_NAME)){
+                    caTarget= target;
+                }
+            }catch(DebugException ex) {}
+        }
         
+        if(caTarget != null){
+            try{
+                caTarget.terminate();
+            }catch(DebugException ex){
+                logger.error("Error while attempting to stop the central agent.", ex);
+            }
+        }
     }
 
-    public void init(IWorkbenchWindow window) {
-        // TODO Auto-generated method stub
-        
-    }
+    public void selectionChanged(IAction action, ISelection selection) { }
+    public void dispose() { }
+    public void init(IWorkbenchWindow window) { }
 
 }
