7e3af46431199b77930fe006f9670b1fad8454af
gmega <giuliano.mega@gmail.com>
2006-09-16 13:41:39 +0000

    - Added IRemotable to this package.
    git-svn-id: svn://localhost/tmp/repo1/trunk@290 70b6eb62-b118-4aea-92db-fdb7de3615d6

---
+++ GOD_Process_Controller_RMI/src/main/java/ddproto1/controller/interfaces/internal/IRemotable.java
@@ -0,0 +1,14 @@
+/*
+ * Created on Jun 21, 2006
+ * 
+ * file: IRemotable.java
+ */
+package ddproto1.controller.interfaces.internal;
+
+import java.rmi.NoSuchObjectException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface IRemotable {
+    public Remote getProxyAndActivate() throws RemoteException, NoSuchObjectException;
+}
