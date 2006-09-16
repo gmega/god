ff3cc67772a2446a0c91a681cb1f615038a303c6
gmega <giuliano.mega@gmail.com>
2006-09-16 13:51:38 +0000

    - Refactored previous tests, added tests for each of the shutdown policies and an extra test to check if the server really timeouts when the timeout is too small.
    git-svn-id: svn://localhost/tmp/repo1/trunk@297 70b6eb62-b118-4aea-92db-fdb7de3615d6

--- GOD_Process_Controller_RMI/src/test/ddproto1/controller/remote/test/MultiDeathTest.java
+++ GOD_Process_Controller_RMI/src/test/ddproto1/controller/remote/test/MultiDeathTest.java
@@ -5,12 +5,15 @@
  */
 package ddproto1.controller.remote.test;
 
-import java.util.List;
+import java.rmi.RemoteException;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
 import junit.framework.TestCase;
 
-import org.apache.log4j.BasicConfigurator;
-
+import ddproto1.controller.constants.IErrorCodes;
+import ddproto1.controller.exception.ServerRequestException;
 import ddproto1.controller.interfaces.IProcessServer;
 import ddproto1.controller.interfaces.IRemoteProcess;
 import ddproto1.controller.interfaces.LaunchParametersDTO;
@@ -18,63 +21,145 @@ import ddproto1.controller.interfaces.LaunchParametersDTO;
 
 public abstract class MultiDeathTest extends TestCase{
     
+    private static final int SHUTDOWN_TIMEOUT = 30000;
+    private static final int STREAM_PROCESSING_TIMEOUT = 5000;
+    private static final int SHUTDOWN_POLLING_INTERVAL = 2000;
+    
+    private static final Logger logger = Logger.getLogger(MultiDeathTest.class);
+    
     private volatile int pDead;
     protected volatile int nprocs;
     private ControlClientExt globalAgent;
     private IProcessServer pServerImpl;
-   
+    
     public void setUp(){
-        BasicConfigurator.configure();
+        pDead = 0;
     }
     
-    public void testNotificationsWith()
+    /** 
+     * Light test for the process server notification mechanisms. 
+     * 
+     * @throws Exception
+     */
+    public void testNotificationMechanismsLightly()
         throws Exception
     {
-        pDead = 0;
-        
-        LaunchParametersDTO [] lps = new LaunchParametersDTO[nprocs];
-        
-        for(int i = 0; i < nprocs; i++)
-            lps[i] = TestUtils.crankDefaultLaunch(i, (i+1)*500, i);
-        try{
-        		for(LaunchParametersDTO lp : lps ){
-        			IRemoteProcess rpprx = 
-        				castToRemoteProcess(getPServerImpl().launch(lp));
-        			System.out.println("Launched process with handle: " + rpprx.getHandle());
-        		}
-        }catch(Exception ex){
-        		ex.printStackTrace();
-        }
-        
-        long startTime = System.currentTimeMillis();
-        
-        synchronized(this){
-            while(pDead < nprocs){
-                wait();
-                if(System.currentTimeMillis() - startTime >= 30000)
-                    fail("Test timeouted.");
+        LaunchParametersDTO[] lps = new LaunchParametersDTO[nprocs];
+
+        for (int i = 0; i < nprocs; i++)
+            lps[i] = TestUtils.crankDefaultLaunch(i, (i + 1) * 500, i);
+        try {
+            for (LaunchParametersDTO lp : lps) {
+                IRemoteProcess rpprx = castToRemoteProcess(getPServerImpl()
+                        .launch(lp));
+                System.out.println("Launched process with handle: "
+                        + rpprx.getHandle());
             }
-            
-            /** Wait to receive all strings. */
-            wait(5000);
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
-        
+
+        // Wait for spontaneous death
+        pollProcessesDeath();
+
         assertTrue(getGlobalAgent().isDone());
-        
-        for(Object rp : getPServerImpl().getProcessList()){
+
+        for (Object rp : getPServerImpl().getProcessList()) {
             IRemoteProcess rpprx = castToRemoteProcess(rp);
-            assertTrue(!rpprx.isAlive());       
+            assertTrue(!rpprx.isAlive());
             rpprx.dispose();
         }
         
+        pollProcessesDeath();
+
         assertTrue(getPServerImpl().getProcessList().size() == 0);
     }
     
-    public void testForcedKill()
+    /**
+     * Test for per-process shutdown requests. 
+     * 
+     * @throws Exception
+     */
+    public void testPerProcessKill()
         throws Exception
     {
-        pDead = 0;
+        startEthernalProcesses(true);
         
+        for(Object rp : getPServerImpl().getProcessList()){
+            IRemoteProcess rprx = castToRemoteProcess(rp);
+            assertTrue(rprx.isAlive());
+            rprx.dispose();
+        }
+        
+        pollProcessesDeath();
+
+        assertTrue(getGlobalAgent().isDone());
+        assertTrue(getPServerImpl().getProcessList().size() == 0);
+    }
+    
+    /** 
+     * Tests full shutdown. 
+     */
+    public void testServerKillWithInfiniteTimeout() throws Exception{
+        startEthernalProcesses(true);
+        
+        Thread.sleep(STREAM_PROCESSING_TIMEOUT);
+        
+        /** Completely synchronous shut down. Method should only return
+         * after all notifications have been processed, so we shouldn't
+         * have to poll for death.
+         */ 
+        getPServerImpl().shutdownServer(true, 0);
+        assertEquals(nprocs, pDead);
+        assertTrue(getGlobalAgent().isDone());
+    }
+    
+    /** 
+     * Tests full shutdown. 
+     */
+    public void testAsynchronousServerKill() throws Exception{
+        startEthernalProcesses(true);
+        getPServerImpl().shutdownServer(true, -1);
+        pollProcessesDeath();
+        assertTrue(getGlobalAgent().isDone());
+    }
+    
+    /** 
+     * Tests full shutdown. 
+     */
+    public void testServerKillWithUnreasonableTimeout() throws Exception{
+        startEthernalProcesses(true);
+        try{
+            getPServerImpl().shutdownServer(true, 1);
+            fail("Server did not timeout.");
+        }catch(RemoteException ex){
+            assertTrue(ex.getCause() instanceof ServerRequestException);
+            ServerRequestException sre = (ServerRequestException) ex.getCause();
+            assertTrue(sre.getDetail() == IErrorCodes.TIMEOUT);
+        }
+    }
+    
+    public void testServerKillWithReasonableTimeout() throws Exception{
+        startEthernalProcesses(true);
+        try{
+            getPServerImpl().shutdownServer(true, SHUTDOWN_TIMEOUT);
+        }catch(RemoteException ex){
+            logger.error(ex);
+            if(ex.getCause() instanceof ServerRequestException){
+                ServerRequestException sre = (ServerRequestException) ex.getCause();
+                if(sre.getDetail() == IErrorCodes.TIMEOUT)
+                    fail("Server timed out.");
+            }
+            fail();
+        }
+        assertEquals(nprocs, pDead);
+        assertTrue(getGlobalAgent().isDone());
+    }
+
+    
+    private void startEthernalProcesses(boolean waitStreamProcessingTimeout)
+        throws Exception
+    {
         LaunchParametersDTO [] lps = new LaunchParametersDTO[nprocs];
         
         for(int i = 0; i < nprocs; i++)
@@ -84,40 +169,23 @@ public abstract class MultiDeathTest extends TestCase{
             getPServerImpl().launch(lp);
         }
         
+        if(waitStreamProcessingTimeout){
+            synchronized(this){
+                wait(STREAM_PROCESSING_TIMEOUT);
+            }
+        }
+    }
+    
+    private void pollProcessesDeath() throws Exception{
         long startTime = System.currentTimeMillis();
         
-        List listCopy = 
-            getPServerImpl().getProcessList();
-        
-        /** Wait for strings to be sent before killing processes. */
-        synchronized(this){
-            wait(5000);
-        }
-        
-        for(Object rp : getPServerImpl().getProcessList()){
-            IRemoteProcess rprx = castToRemoteProcess(rp);
-            assertTrue(rprx.isAlive());
-            rprx.dispose();
-        }
-        
-        synchronized(this){
-            while(pDead < nprocs){
-                wait();
-                if(System.currentTimeMillis() - startTime >= 30000)
-                    fail("Test timeouted.");
+        while(pDead < nprocs){
+            synchronized(this){
+                wait(SHUTDOWN_POLLING_INTERVAL);
             }
-            wait(5000);
+            if(System.currentTimeMillis() - startTime >= SHUTDOWN_TIMEOUT)
+                fail("Test timeouted.");
         }
-        
-        assertTrue(getGlobalAgent().isDone());
-
-// Doesn't make sense for this test. The call to 'dispose' destroys the remote objects.
-//        for(Object rp : listCopy){
-//            RemoteProcessPrx rprx = castToPrx(rp);
-//            assertTrue(!rprx.isAlive());            
-//        }
-        
-        assertTrue(getPServerImpl().getProcessList().size() == 0);
     }
     
     protected synchronized void notifyDeath(){
