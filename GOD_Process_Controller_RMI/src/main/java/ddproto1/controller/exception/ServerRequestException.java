493ec441f90bd9c6f190d02d74293e1dbb7f4740
gmega <giuliano.mega@gmail.com>
2006-09-16 13:38:00 +0000

    - Added "detail" field to indicate the cause of the remote exception without having to serialize extra stuff (and thus without requiring the client to have extra stuff to deserialize the exception). - Added extra constructor to inject the detail information.
    git-svn-id: svn://localhost/tmp/repo1/trunk@287 70b6eb62-b118-4aea-92db-fdb7de3615d6

--- GOD_Process_Controller_RMI/src/main/java/ddproto1/controller/exception/ServerRequestException.java
+++ GOD_Process_Controller_RMI/src/main/java/ddproto1/controller/exception/ServerRequestException.java
@@ -7,15 +7,23 @@ package ddproto1.controller.exception;
 
 import java.rmi.RemoteException;
 
-public class ServerRequestException extends RemoteException {
+import ddproto1.controller.constants.IErrorCodes;
 
-    /**
-     * 
-     */
+public class ServerRequestException extends RemoteException implements IErrorCodes{
+
     private static final long serialVersionUID = 5760342086640630411L;
 
-    public ServerRequestException() { super(); }
-    public ServerRequestException(String s, Throwable cause) { super(s, cause); }
-    public ServerRequestException(String s) { super(s); }
+    private final int fDetail;
+
+    public ServerRequestException(String message, Throwable cause, int detail){
+        super(message, cause);
+        fDetail = detail;
+    }
+    
+    public ServerRequestException() { this(null, null, UNSPECIFIED); }
+    public ServerRequestException(String s, Throwable cause) { this(s, cause, UNSPECIFIED); }
+    public ServerRequestException(String s) { this(s, null, UNSPECIFIED); }
+    
+    public int getDetail() { return fDetail; }
 
 }
