0eb9fd2b356b62383f4e158ed1293db13814b8eb
gmega <giuliano.mega@gmail.com>
2006-09-16 13:36:37 +0000

    - Documentation remade. - Added extra constants for describing timeout errors at shutdown.
    git-svn-id: svn://localhost/tmp/repo1/trunk@286 70b6eb62-b118-4aea-92db-fdb7de3615d6

--- GOD_Process_Controller_RMI/src/main/java/ddproto1/controller/constants/IErrorCodes.java
+++ GOD_Process_Controller_RMI/src/main/java/ddproto1/controller/constants/IErrorCodes.java
@@ -13,18 +13,38 @@ package ddproto1.controller.constants;
  */
 public interface IErrorCodes {
     /**
-     * Bad parameter. Means that one or more parameters
-     * (passed to a method) are invalid.
+     * Indicates no failure.
+     */
+    public static final int STATUS_OK = 0;
+    
+    /**
+     * Indicates a failed request.
+     */
+    public static final int STATUS_ERROR = 1;
+    
+    /**
+     * Bad parameter detail. Means that the request failed 
+     * because one or more parameters (passed to a method) 
+     * are invalid.
      */
     public static final int INVALID_PARAMETER = -1;
     
     /**
-     * Request couldn't be completed due to an I/O error
-     * at the server side.
+     * I/O failure detail. Means that the request failed 
+     * due to an I/O error at the server side.
      */
     public static final int IO_FAILURE = -2;
     
+    /**
+     * Timeout failure detail. Means that the request failed
+     * because of a timeout at the server side.
+     */
+    public static final int TIMEOUT = -3;
     
-    public static final int STATUS_OK = 0;
-    public static final int STATUS_ERROR = 1;
+    /**
+     * Unspecified failure detail. The source of the failure
+     * didn't specify why it failed.
+     */
+    public static final int UNSPECIFIED = -4;
+
 }
