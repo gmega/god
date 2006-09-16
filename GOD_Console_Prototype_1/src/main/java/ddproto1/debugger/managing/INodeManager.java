97109e4c5f741a03c66d4ed8eb9753611e10dae2
gmega <giuliano.mega@gmail.com>
2006-09-16 14:15:11 +0000

    - Added a final empty array of IStackFrame to be returned by the node debug targets.
    git-svn-id: svn://localhost/tmp/repo1/trunk@316 70b6eb62-b118-4aea-92db-fdb7de3615d6

--- GOD_Console_Prototype_1/src/main/java/ddproto1/debugger/managing/INodeManager.java
+++ GOD_Console_Prototype_1/src/main/java/ddproto1/debugger/managing/INodeManager.java
@@ -5,7 +5,12 @@
  */
 package ddproto1.debugger.managing;
 
+import org.eclipse.debug.core.model.IStackFrame;
+
 public interface INodeManager {
+    
+    public static final IStackFrame[] NO_CALLSTACK = new IStackFrame[0];
+    
     public String getName();
     public IThreadManager getThreadManager();
 }
